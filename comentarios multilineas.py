438fe2d3c9dd7c900b8bf253a9a3dee1421a424d
Roberto Shiao <95731527+Shiao-Li@users.noreply.github.com>
2022-07-11 21:52:52 -0500

    Add files via upload

---
+++ comentarios multilineas.py
@@ -0,0 +1,19 @@
+'''DENTRO DE ESTE LAS COMILLAS DOBLES SE PODRA ESCRIBIR 
+EL COMENTARIO QUE EL DESARROLLADOR DESEE, PUEDE SERVIR PARA DOCUMENTAR EL CODIGO
+'''
+
+""" EN LA SIGUIENTE LINEA DE CODIGO 
+SE IMPIMIRA UN HOLA MUNDO"""
+print(" >>> HOLA MUNDO <<<")
+
+'''
+realizar un programa que sume dos nueros enteros
+estos dos numoers tendran que ser 4 y 5
+'''
+suma = 4 + 5
+print(f"La suma de 4 y 5 es: {suma}" ) 
+
+"""
+en la linea 14 y 15 se esta realizando una operacion
+matematica 
+"""
---
+++ comentarios una linea.py
@@ -0,0 +1,10 @@
+# en este caso al iniciar la linea de codigo con "#" solo se podra escribir el comentario en una sola linea 
+
+print(" >>> HOLA MUNDO <<<") #en esta linea solo se impimira un hola mundo 
+
+suma = 4 + 5 # en esta linea se esta declarando la variable suma 
+print(f"La suma de 4 y 5 es: {suma}" )  #aqui se esta imprimiento el resultado de la variable suma 
+
+
+# como se pudo observar en este ejemplo gracias al caracter numeral 
+# se puede ir documentando linea por linea
